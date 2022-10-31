ed2b769f686cf1357de18095a503cd1c887e6273
shagish <berginglobal@gmail.com>
2022-10-31 20:11:43 +0530

    modified Project Structure

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 #! /bin/python3
-import random  
-import string 
-import json
-from urllib import response
-from flask import Flask, render_template, request, redirect, url_for,session
+
+import random
+import string
+from flask import Flask, render_template, request, redirect, url_for, session
 import ibm_db
+
+
+def Upper_Lower_string(length):
+    result = ''.join((random.choice(string.ascii_uppercase)
+                     for x in range(length)))  # run the loop until the define length
+    return result
+
 
 conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=125f9f61-9715-46f9-9399-c8177b21803b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30426;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=wvn94274;PWD=2K5Z7ZiQuEV2edmQ", '', '')
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'helloworld'
+
 
 @app.route('/')
 def home():
@@ -55,27 +62,35 @@ def login():
         ibm_db.bind_param(log_stmt, 2, password)
         ibm_db.execute(log_stmt)
         rs = ibm_db.fetch_assoc(log_stmt)
-        if rs :
-            role=session['role']='user'
-            name=session['name']=name
+        if rs:
+            session['role'] = 'user'
+            session['name'] = rs['USERNAME']
+            session['ticket'] = rs['TICKET']
+            session['review'] = rs['REVIEW_STATUS']
+            session['query'] = rs['QUERY']
             print(rs)
-            return render_template('cus_dashboard.html',role=role,name=name,j=rs)
+            return render_template('dashboard.html',
+                                   role=session['role'],
+                                   name=session['name'],
+                                   ticket=session['ticket'],
+                                   review=session['review'],
+                                   query=session['query'])
         log_stmt = ibm_db.prepare(
             conn, 'SELECT * FROM agent WHERE username=? and password=?')
         ibm_db.bind_param(log_stmt, 1, name)
         ibm_db.bind_param(log_stmt, 2, password)
         ibm_db.execute(log_stmt)
         rs = ibm_db.fetch_assoc(log_stmt)
-        if rs :
-            return render_template('agt_dashboard.html')
+        if rs:
+            return render_template('dashboard.html')
         log_stmt = ibm_db.prepare(
             conn, 'SELECT * FROM admin WHERE username=? and password=?')
         ibm_db.bind_param(log_stmt, 1, name)
         ibm_db.bind_param(log_stmt, 2, password)
         ibm_db.execute(log_stmt)
         rs = ibm_db.fetch_assoc(log_stmt)
-        if rs :
-            return render_template('adm_dashboard.html')
+        if rs:
+            return render_template('dashboard.html')
         else:
             msg = 'UID/Password is incorrect'
             return render_template('login.html', msg=msg)
@@ -83,26 +98,30 @@ def login():
         return render_template('login.html')
 
 
-@app.route('/dashboard', methods=['POST','GET'])
-def usr_dashboard():
-    return render_template('cus_dashboard.html')
+@app.route('/dashboard', methods=['POST', 'GET'])
+def dashboard():
+    return render_template('dashboard.html')
 
 
-@app.route('/dashboard',methods=['POST','GET'])
-def agt_dashboard():
-    return render_template()
-
-
-@app
+@app.route('/success', methods=['POST', 'GET'])
+def success():
+    if request.method == "POST":
+        ticket = session['ticket'] = Upper_Lower_string(16)
+        print(ticket, session['ticket'])
+        query = request.form['query']
+        sql = "UPDATE user SET QUERY=?,TICKET=? WHERE USERNAME=?"
+        out = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(out, 1, query)
+        ibm_db.bind_param(out, 2, session['ticket'])
+        ibm_db.bind_param(out, 3, session['name'])
+        status = ibm_db.execute(out)
+        if status:
+            msg = 'Success ! Your Ticket Nno is :', ticket, 'You can now return to the home page'
+            return render_template('success.html', msg=msg)
+        else:
+            msg = 'Error Submitting your Query'
+            return render_template('success.html', msg=msg)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# generate random string
-
-def Upper_Lower_string(length): 
-    result = ''.join((random.choice(string.ascii_lowercase) for x in range(length))) # run loop until the define length  
-    print(" Random string generated in Lowercase: ", result)
-    result1 = ''.join((random.choice(string.ascii_uppercase) for x in range(length))) # run the loop until the define length  
-    print(" Random string generated in Uppercase: ", result1)
--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,11 @@
-function form_create(){
-    var elem=document.getElementById('add_elem')
-    elem.innerHTML=`<div class=\"form-group\"><form action=\"url_for()\" method=\"post\"><label class=\"col-md-5\">Enter your query : </label><input class=\"form-control col-md-5\"
-            type=\"textarea\" name=\"query\" id=\"query\">
-        <input type=\"submit\" value=\"Submit\">
-    </form>
-</div>`
+function form_create() {
+    var CONTINUE = confirm("DOING THIS WILL DELETE YOUR PREVIOUS QUERY.Do you want to continue?")
+    if (CONTINUE === true) {
+        var elem = document.getElementById('add_elem')
+        elem.innerHTML = `<div class="container pd-20"><form action="/success" method="post">
+        <div class="form-group"><label class="col-md-5">Enter your query : </label>
+        <input name="query" class=\"form-control col-md-5\"type="textarea" name="query" id="query">
+        <input type="submit" value="Submit"></div></form></div>`
+    }
+
 }
